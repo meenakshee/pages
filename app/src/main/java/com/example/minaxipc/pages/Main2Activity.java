4e10f6d200eaf6588cdd53503016feae34b875dc
meenakshee <durby1892@gmail.com>
2016-08-02 00:49:48 -0700

    Initial commit

---
+++ app/src/main/java/com/example/minaxipc/pages/Main2Activity.java
@@ -0,0 +1,17 @@
+package com.example.minaxipc.pages;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+        /** Called when the user clicks the Send button */
+        Intent intent = getIntent();
+
+    }
+}
---
+++ app/src/main/java/com/example/minaxipc/pages/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.minaxipc.pages;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+    Button Btn1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
